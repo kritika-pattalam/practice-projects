fb042c97bdb592f46cb40c951d01aa5e46e3fc1c
Kritika <kritikarvin2308@gmail.com>
2022-01-02 21:22:10 -0500

    React getting started

---
+++ react-info-site/index.js
@@ -0,0 +1,15 @@
+const navbar = (
+    <nav>
+        <h1>Kritika's Portfolio</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+)
